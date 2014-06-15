7e9a7528b74b52ba5e3585558a7f7440967cbe65
bootchk <bootch@nc.rr.com>
2014-06-15 11:36:10 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='qtGestureFramework',
+      version='1.0.0',
+      description='Classes supporting Qt gestures',
+      author='Lloyd Konneker',
+      author_email='bootch@nc.rr.com',
+      url='https://github.com/bootchk/qtGestureFramework',
+      packages=['qtGestureFramework', 
+                'qtGestureFramework.customGesture',
+                ],
+     )
