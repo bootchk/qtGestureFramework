8b1f1eee1ed59e30e041b8483502a0cca7c59b57
bootchk <bootch@nc.rr.com>
2014-09-28 12:49:06 -0400

    Setup.py reflect last commit, and not include demoApp

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(name='qtGestureFramework',
       author_email='bootch@nc.rr.com',
       url='https://github.com/bootchk/qtGestureFramework',
       packages=['qtGestureFramework',
+                'qtGestureFramework.gestureHandler'
                 'qtGestureFramework.gestureable',
                 'qtGestureFramework.customGesture',
-                'demoApp',
                 ],
      )
